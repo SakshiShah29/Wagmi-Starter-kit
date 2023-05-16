2d3fb3de650101f1d3b242dd3b54a5f9d4d57b3e
SakshiShah29 <sakshi.shah299@gmail.com>
2023-05-16 20:45:35 +0530

    Starter kit created

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,29 @@ import { ConnectButton } from '@rainbow-me/rainbowkit';
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
+import { useAccount, useBalance, useConnect, useNetwork } from 'wagmi';
+import { useEffect, useState } from 'react';
 
 const Home: NextPage = () => {
+
+ const {address, connector:activeConnector,isConnected}=useAccount();
+
+ const {error}=useConnect();
+
+ const {chain,chains}=useNetwork();
+
+ const {data}=useBalance({
+  address,
+ });
+
+ const [hasMounted, setHasMounted] = useState(false);
+ useEffect(() => {
+   setHasMounted(true);
+ }, []);
+ if(!hasMounted){
+  return null;
+ }
+
   return (
     <div className={styles.container}>
       <Head>
@@ -16,67 +37,38 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
+        <div className='container'>
         <ConnectButton />
-
-        <h1 className={styles.title}>
-          Welcome to <a href="">RainbowKit</a> + <a href="">wagmi</a> +{' '}
-          <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a className={styles.card} href="https://rainbowkit.com">
-            <h2>RainbowKit Documentation &rarr;</h2>
-            <p>Learn how to customize your wallet connection flow.</p>
-          </a>
-
-          <a className={styles.card} href="https://wagmi.sh">
-            <h2>wagmi Documentation &rarr;</h2>
-            <p>Learn how to interact with Ethereum.</p>
-          </a>
-
-          <a
-            className={styles.card}
-            href="https://github.com/rainbow-me/rainbowkit/tree/main/examples"
-          >
-            <h2>RainbowKit Examples &rarr;</h2>
-            <p>Discover boilerplate example RainbowKit projects.</p>
-          </a>
-
-          <a className={styles.card} href="https://nextjs.org/docs">
-            <h2>Next.js Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a
-            className={styles.card}
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-          >
-            <h2>Next.js Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            className={styles.card}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        {isConnected?(
+          <h2 className='activeConnector'>
+            You are connected to <span>{activeConnector?.name}</span>{' '}
+          </h2>
+        ):(
+          <h2>Not Connected</h2>
+        )}
         </div>
+
+        <section className='Balance'>
+          <h3>
+            Balance: {" "}
+            <span>
+              {Number(data?.formatted).toFixed(3)}{data?.symbol}
+            </span>
+
+          </h3>{" "}
+        </section>
+
+        {chains && 
+        <div className='Current_chain'>
+          Current chain:
+          {isConnected?(
+            <span>{chains.map((chain)=>chain.name)}</span>
+          ):("Connect Wallet")}</div>}
+        
       </main>
 
-      <footer className={styles.footer}>
-        <a href="https://rainbow.me" rel="noopener noreferrer" target="_blank">
-          Made with ❤️ by your frens at 🌈
-        </a>
-      </footer>
+      <p className="error">{error && <span>{error.message}</span>}</p>
+      {/* End of component */}
     </div>
   );
 };
